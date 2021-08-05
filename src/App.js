c82cbf3310b5288b8cdb0f37bf2e2f21088bf8c4
nirbar <barzilai.nir@gmail.com>
2021-08-05 15:07:52 +0300

    finishing sign in  handleSubmit method

--- src/App.js
+++ src/App.js
@@ -72,7 +72,7 @@ class App extends React.Component {
               console.log(this.state);
             } */
           );
-          console.log(this.state);
+          //console.log(this.state);
         });
       }
       //createUserProfileDocument(user);
