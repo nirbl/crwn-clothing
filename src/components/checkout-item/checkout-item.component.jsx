2f8335ff926e5ea76441ddd2c6592bffd81e69a8
nirbar <barzilai.nir@gmail.com>
2021-08-09 21:21:04 +0300

    added addItem and removeItem functionality to checkout item component

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,17 +1,51 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
+import {
+  clearItemFromCart,
+  addItem,
+  removeItem,
+} from '../../redux/cart/cart.actions';
 
 import './checkout-item.styles.scss';
 
-const CheckoutItem = ({ cartItem: { name, imageUrl, price, quantity } }) => (
-  <div className='checkout-item'>
-    <div className='image-container'>
-      <img src={imageUrl} alt='item' />
+//const CheckoutItem = ({ cartItem: { name, imageUrl, price, quantity } }) => (
+// instead of destructuring these values above line -
+// (since we don't have access to the item property).
+// so we need to do explicit "return" in this function - and make sure to set the context of // these values inside of the squiggly (שרבוט) lines of our function definition (by
+// destructuring 2 lines below)
+// and now we'll have access to our cartItem, as well as our new clearItem that
+// we're passing in
+const CheckoutItem = ({ cartItem, clearItem, addItem, removeItem }) => {
+  const { name, imageUrl, price, quantity } = cartItem;
+  return (
+    <div className='checkout-item'>
+      <div className='image-container'>
+        <img src={imageUrl} alt='item' />
+      </div>
+      <span className='name'>{name}</span>
+      <span className='quantity'>
+        <div className='arrow' onClick={() => removeItem(cartItem)}>
+          &#10094;
+        </div>
+        <span className='value'>{quantity}</span>
+        <div className='arrow' onClick={() => addItem(cartItem)}>
+          &#10095;
+        </div>
+      </span>
+      <span className='price'>{price}</span>
+      <span className='remove-button' onClick={() => clearItem(cartItem)}>
+        &#10005;
+      </span>
     </div>
-    <span className='name'>{name}</span>
-    <span className='quantity'>{quantity}</span>
-    <span className='price'>{price}</span>
-    <span className='remove-button'>&#10005;</span>
-  </div>
-);
+  );
+};
+//);
 
-export default CheckoutItem;
+const mapDispatchToProps = (dispatch) => ({
+  clearItem: (item) => dispatch(clearItemFromCart(item)),
+  addItem: (item) => dispatch(addItem(item)),
+  removeItem: (item) => dispatch(removeItem(item)),
+});
+
+export default connect(null, mapDispatchToProps)(CheckoutItem);
--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -22,3 +22,19 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
   // "quantity" =  quantity property gets attached the first time around
   //               since this if block (above!) won't run when it's a new item !
 };
+
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+  const existingCartItem = cartItems.find(
+    (cartItem) => cartItem.id === cartItemToRemove.id
+  );
+
+  if (existingCartItem.quantity === 1) {
+    return cartItems.filter((cartItem) => cartItem.id !== cartItemToRemove.id);
+  }
+
+  return cartItems.map((cartItem) =>
+    cartItem.id === cartItemToRemove.id
+      ? { ...cartItem, quantity: cartItem.quantity - 1 }
+      : cartItem
+  );
+};
